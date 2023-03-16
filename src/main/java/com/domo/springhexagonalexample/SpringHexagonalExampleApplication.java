3d47ba3ebca9255efbcb3daf5a005942c013db77
kdomo <dongho5088@naver.com>
2023-03-16 16:56:08 +0900

    init: project initial

---
+++ src/main/java/com/domo/springhexagonalexample/SpringHexagonalExampleApplication.java
@@ -0,0 +1,13 @@
+package com.domo.springhexagonalexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHexagonalExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHexagonalExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domo/springhexagonalexample/SpringHexagonalExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domo.springhexagonalexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHexagonalExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
